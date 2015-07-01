34cdbdb00450d3acd8c49e52e6ed067f1a58bec1
JoelT <tohsingchang@gmail.com>
2015-07-01 03:00:07 -0400

    validations added

--- app/models/supperplace.rb
+++ app/models/supperplace.rb
@@ -1,4 +1,10 @@
 class Supperplace < ActiveRecord::Base
   has_many :photos
   has_many :opening_hours
+
+  validates :address, presence: true
+  validates :crusine, presence: true
+  validates :state, presence: true, inclusion: { in: 1..2 }
+  validates :phone, length: { is: 8 }, allow_blank: true
+
 end
