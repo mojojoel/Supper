a6ff80cd6fa46ce402a624f774fc66d4d9016949
JoelT <tohsingchang@gmail.com>
2015-07-01 16:38:37 -0400

    readded Devise for AA, initial AA install

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'spring',        group: :development
 
 # ActiveAdmin
 gem 'activeadmin', github: 'activeadmin'
+gem 'devise'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :supperplaces, :photos, :opening_hours
   #resources :supperplaces do
   #  resources :photos, :opening_hours, shallow: true
